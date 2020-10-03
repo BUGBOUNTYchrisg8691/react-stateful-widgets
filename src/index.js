7562d72863fea8c8e9820c5ce31a655afef6829f
Christopher Girvin <bugbountychrisg8691@gmail.com>
2020-10-03 11:50:30 -0400

    struggling with stretch

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import Moods from './components/Moods';
 import Spinner from './components/Spinner';
 import Squares from './components/Squares';
 import Programmers from './components/Programmers';
-import Todos from './components/Todos';
 
 render(
   <>
@@ -17,7 +16,6 @@ render(
     <Input />
     <Squares />
     <Programmers />
-    <Todos />
   </>
   ,
   document.querySelector('#root')
